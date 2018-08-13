a5e1b6357d01e242fd244e614ce5270f135be8e6
Guerlak <rafael.b.leite@outlook.com>
2018-08-14 00:48:40 +0100

    update server url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const profile = require('./controllers/profile')
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: true
     }
   });
