714d90d6f1da8f60c6556d8d2e09c7a5bf09417b
Guerlak <rafael.b.leite@outlook.com>
2018-08-15 01:47:21 +0100

    update server

--- controllers/login.js
+++ controllers/login.js
@@ -5,16 +5,16 @@ const handleLogin = (req, res, db, crypt)=>{
       db.select('email', 'hash').from('login')
       .where('email', email)
       .then(data => {
-          const isValid = crypt.compareSync(password, data[0].hash)
-          if(isValid){
-              db.select('*').from('users')
-              .where('email', '=', req.body.email)
-              .then(user => {
-                  res.json(user[0])
-              }).catch(err => res.status(400).json("Unable to find user on login, try again..."))
-            }else{
-                res.status(400).json("Wrong credentials...")
-            }
+            const isValid = crypt.compareSync(password, data[0].hash)
+            if(isValid){
+                db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                }).catch(err => res.status(400).json("Unable to find user on login, try again..."))
+                }else{
+                    res.status(400).json("Wrong credentials...")
+                }
             })
       .catch(err => res.status(400).json("Wrong credentials..."))
     }
